08d9f2fa159daf402324760a0afe7881a84e1767
arunkuttiyara <arunkuttiyara@yahoo.com>
2016-04-02 23:12:30 -0400

    modyfying few comments

--- networking/packet_penetration.py
+++ networking/packet_penetration.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 #Packet sniffer in python
 #For Linux
-# test
 
 import socket
  
+
 #create an INET, raw socket
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
  
-# receive a packet
+# receive a packet and display it
 while True:
  print s.recvfrom(65565)
 
